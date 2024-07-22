69b942bdcaa0d74b68f621e6874b0e660c65bb37
Ahmed <ahmed.sh5121999@gmail.com>
2024-07-22 23:45:05 +0300

    add user-profile component and edit on it and fix some issues in routing

--- zaid/src/User/user.routing.ts
+++ zaid/src/User/user.routing.ts
@@ -1,7 +1,19 @@
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileManagementComponent } from './Components/Profile-Management/Profile-Management.component';
+import { MyProfileComponent } from './Components/Profile-Management/my-profile/my-profile.component';
+import { PaymentComponent } from './Components/Profile-Management/payment/payment.component';
+import { ProfileSettingComponent } from './Components/Profile-Management/profile-setting/profile-setting.component';
+import { ShippingComponent } from './Components/Profile-Management/shipping/shipping.component';
+import { VerifyIdentityComponent } from './Components/Profile-Management/verify-identity/verify-identity.component';
 
 const routes: Routes = [
-  {  },
+    { path: 'my', component: ProfileManagementComponent, children: [
+      { path: 'profile', component: MyProfileComponent },
+      { path: 'payment', component: PaymentComponent },
+      { path: 'profile-setting', component: ProfileSettingComponent },
+      { path: 'shipping', component: ShippingComponent },
+      { path: 'verify-identity', component: VerifyIdentityComponent },
+    ] },
 ];
 
 export const UserRoutes = RouterModule.forChild(routes);
--- zaid/src/app/app.module.ts
+++ zaid/src/app/app.module.ts
@@ -2,25 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserModule } from '../User/User.module';
-import { ActionModule } from '../Action/Action.module';
-import { ItemsModule } from '../Items/Items.module';
-import { SharedModule } from '../Shared/Shared.module';
-import { AdminModule } from '../Admin/Admin.module';
 import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserModule,
-    ActionModule,
-    ItemsModule,
-    SharedModule,
-    AdminModule,
     ReactiveFormsModule,
   ],
   providers: [
