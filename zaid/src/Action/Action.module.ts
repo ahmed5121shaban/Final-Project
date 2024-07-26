4775795d60ca53700a8295f81d99f0dd92e42d71
Sara Abdalla <159088471+SaraMohameddSayed@users.noreply.github.com>
2024-07-27 01:24:54 +0300

    deleteconfirmation component creation

--- zaid/src/Action/Action.module.ts
+++ zaid/src/Action/Action.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActionComponent } from './Action.component';
 import { AuctionDetailsComponent } from './Components/auction-details/auction-details.component';
+import { DeleteConfirmationComponent } from './Components/delete-confirmation/delete-confirmation.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ActionComponent,AuctionDetailsComponent],
-  exports:[AuctionDetailsComponent]
+  declarations: [ActionComponent,AuctionDetailsComponent,DeleteConfirmationComponent],
+  exports:[AuctionDetailsComponent,DeleteConfirmationComponent]
 })
 export class ActionModule { }
