e36ef27338148d68742ec72750df7775bef86c75
Ahmed Shaban <105598769+ahmed5121shaban@users.noreply.github.com>
2024-09-03 14:53:41 +0300

    add dashboard home page

--- zaid/src/Admin/admin.routing.ts
+++ zaid/src/Admin/admin.routing.ts
@@ -1,7 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardHomeComponent } from './Components/dashboard-home/dashboard-home.component';
 
 const routes: Routes = [
-  {  },
+  { path:"home",component:DashboardHomeComponent },
 ];
 
 export const AdminRoutes = RouterModule.forChild(routes);
