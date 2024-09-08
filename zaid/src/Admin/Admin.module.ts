add8c80c0efa27734d151d0f82961ac6f3c5e8c8
Sara Abdalla <159088471+SaraMohameddSayed@users.noreply.github.com>
2024-09-09 02:27:01 +0300

    auction-live-stream added

--- zaid/src/Admin/Admin.module.ts
+++ zaid/src/Admin/Admin.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './Admin.component';
+import { AuctionLiveStreamComponent } from './Components/auction-live-stream/auction-live-stream.component';
+import { AdminRoutes } from './admin.routing';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutes
   ],
-  declarations: [AdminComponent]
+  declarations: [AdminComponent,AuctionLiveStreamComponent]
 })
 export class AdminModule { }
--- zaid/src/Admin/admin.routing.ts
+++ zaid/src/Admin/admin.routing.ts
@@ -1,7 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
+import { AuctionLiveStreamComponent } from './Components/auction-live-stream/auction-live-stream.component';
 
 const routes: Routes = [
-  {  },
+  { path: 'auction-live-stream', component: AuctionLiveStreamComponent }
 ];
 
 export const AdminRoutes = RouterModule.forChild(routes);
