ab8ec48c38c0d8a1c82ed971f983aa264f0968b6
SaraMohameddSayed <saramohamedsci884@gmail.ccom>
2024-10-25 02:18:22 +0300

    earnings and withdraw integration

--- zaid/src/User/Components/Profile-Management/seller-earnings/seller-earnings.component.ts
+++ zaid/src/User/Components/Profile-Management/seller-earnings/seller-earnings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuctionService } from '../../../../Action/Services/auction.service';
 
 @Component({
   selector: 'app-seller-earnings',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrl: './seller-earnings.component.css'
 })
 export class SellerEarningsComponent {
+  withdrawnbalance: number = 0;
+  availableBalance: number = 0;
+  upcomingBalance: number = 0;
 
+  constructor(private auctionService: AuctionService) {
+    this.getAvailableBalance();
+    this.getUpcomingBalance();
+    this.getwithdrawnamount();
+
+  }
+
+  getAvailableBalance() {
+    this.auctionService.getAvailableBalance().subscribe({
+      next: (response: any) => {
+        this.availableBalance = response.result.availablebalance;
+        console.log(response);
+
+      },
+      error: (error) => {
+        console.log(error);
+
+      }
+    });
+
+  }
+  getUpcomingBalance() {
+    this.auctionService.getUpComingBalance().subscribe({
+      next: (result: any) => {
+        this.upcomingBalance = result.result;
+        console.log(result);
+
+      },
+      error: (error) => {
+        console.log(error);
+
+      }
+    });
+  }
+  getwithdrawnamount() {
+    this.auctionService.getwithdrawnamount().subscribe({
+      next: (response) => {
+        this.withdrawnbalance = response;
+      },
+      error: (error) => {
+        console.log(error);
+
+      }
+    })
+  }
 }
--- zaid/src/User/Components/Profile-Management/sellerwithdraw/sellerwithdraw.component.ts
+++ zaid/src/User/Components/Profile-Management/sellerwithdraw/sellerwithdraw.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuctionService } from '../../../../Action/Services/auction.service';
 
 @Component({
   selector: 'app-sellerwithdraw',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrl: './sellerwithdraw.component.css'
 })
 export class SellerwithdrawComponent {
+  availableBalance: number=0;
+  amount:number=0;
+  paypalEmail:string="";
+  stripeEmail:string="";
+  constructor(private auctionService: AuctionService) {
+    this.getAvailableBalance();
+
+  }
+
+  getAvailableBalance() {
+    this.auctionService.getAvailableBalance().subscribe({
+      next: (response: any) => {
+        console.log(response);
+        this.availableBalance = response.result.availablebalance;
+        this.paypalEmail=response.result.paymentEmail.paypalEmail;
+        this.stripeEmail=response.result.paymentEmail.stripelEmail;
+        
+      },
+      error:(error)=>{
+console.log(error);
+
+      }
+    });
+    
+  }
+withdraw(){
+  this.auctionService.withdraw(this.amount).subscribe({
+    next:(response)=>{
+      console.log(response);
+      
+    },
+    error:(error)=>{
+console.log(error);
+
+    }
+  })
+}
 
 }
