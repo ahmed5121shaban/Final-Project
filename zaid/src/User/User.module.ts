db751b5a40f8ded688cef6cf37a08c2307d041a8
SaraMohameddSayed <saramohamedsci884@gmail.ccom>
2024-10-24 03:09:31 +0300

    seller paid completed auctions table

--- zaid/src/User/User.module.ts
+++ zaid/src/User/User.module.ts
@@ -29,6 +29,7 @@ import { WonAuctionComponent } from './Components/Profile-Management/won-auction
 import { WaitingAuctionComponent } from './Components/Profile-Management/waiting-auction/waiting-auction.component';
 import { WatchlistComponent } from "./Components/Profile-Management/watchlist/watchlist.component";
 import { NgxPaginationModule } from "ngx-pagination";
+import { CompleteAuctionComponent } from './Components/Profile-Management/complete-auction/complete-auction.component';
 
 
 
@@ -72,6 +73,7 @@ import { NgxPaginationModule } from "ngx-pagination";
     MyLiveAuctionsComponent,
     WaitingAuctionComponent,
     WatchlistComponent,
+    CompleteAuctionComponent
 
   ],
 
