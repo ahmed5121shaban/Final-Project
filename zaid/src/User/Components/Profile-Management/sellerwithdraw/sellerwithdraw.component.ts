22f302cb9a1c0552acde7a381e2bf3546105d445
SaraMohameddSayed <saramohamedsci884@gmail.ccom>
2024-10-26 16:31:53 +0300

    add logic for withdraw payment buttons

--- zaid/src/User/Components/Profile-Management/sellerwithdraw/sellerwithdraw.component.ts
+++ zaid/src/User/Components/Profile-Management/sellerwithdraw/sellerwithdraw.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuctionService } from '../../../../Action/Services/auction.service';
+import { PaymentService } from '../../../../Action/Services/payment.service';
 
 @Component({
   selector: 'app-sellerwithdraw',
@@ -7,12 +8,16 @@ import { AuctionService } from '../../../../Action/Services/auction.service';
   styleUrl: './sellerwithdraw.component.css'
 })
 export class SellerwithdrawComponent {
+  method:number=0;
+  paymentCount:number=0;
   availableBalance: number=0;
   amount:number=0;
   paypalEmail:string="";
   stripeEmail:string="";
-  constructor(private auctionService: AuctionService) {
+  constructor(private auctionService: AuctionService,private paymentService:PaymentService) {
     this.getAvailableBalance();
+    this.getPaymentMethodsCount();
+
 
   }
 
@@ -22,7 +27,7 @@ export class SellerwithdrawComponent {
         console.log(response);
         this.availableBalance = response.result.availablebalance;
         this.paypalEmail=response.result.paymentEmail.paypalEmail;
-        this.stripeEmail=response.result.paymentEmail.stripelEmail;
+        this.stripeEmail=response.result.paymentEmail.stripeEmail;
         
       },
       error:(error)=>{
@@ -45,4 +50,12 @@ console.log(error);
   })
 }
 
+getPaymentMethodsCount(){
+  this.paymentService.IsUserHavePayment().subscribe({
+    next:(res:any)=>{
+       this.paymentCount=res.count;
+       console.log(res)
+     },
+     error:(err)=>{console.log(err)}
+ });}
 }
